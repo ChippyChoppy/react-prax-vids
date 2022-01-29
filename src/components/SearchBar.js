2ba19e9077bb95ecba16a701c20ea792996c3963
ChippyChoppy <rebeccamargaret13@gmail.com>
2022-01-29 17:45:35 -0500

    fixed val without input handler

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,17 @@
 import React from 'react'
 
 class SearchBar extends React.Component {
+  state = {
+    term: 'fixed val baby!',
+  }
+
   render() {
     return (
       <div className="search-bar ui segment">
         <form className="ui form">
           <div className="field">
             <label>Video Search</label>
-            <input type="text" />
+            <input type="text" value={this.state.term} />
           </div>
         </form>
       </div>

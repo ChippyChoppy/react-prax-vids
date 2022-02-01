c75ef9a534ec28d201303b80577aeef70127036b
ChippyChoppy <rebeccamargaret13@gmail.com>
2022-02-01 12:27:33 -0500

    video styling

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,14 @@
-const KEY = 'AIzaSyB-nosK8OCWQSm9druDKG8SkSR2XugrFj0';
+import axios from 'axios'
+
+const KEY = 'AIzaSyB-nosK8OCWQSm9druDKG8SkSR2XugrFj0'
+// uppercase because specifically saying it's constant val, should not be changed unless you really know what you're doing!
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+      part: 'snippet',
+      type: 'video',
+      maxResults: 5,
+      key: KEY
+  }
+});

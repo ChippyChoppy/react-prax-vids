512b1c5547601e56e452fb39703d30c290918415
ChippyChoppy <rebeccamargaret13@gmail.com>
2022-02-03 12:14:08 -0500

    added default video list and setState on default embeded video

--- src/components/App.js
+++ src/components/App.js
@@ -10,13 +10,20 @@ class App extends React.Component {
     selectedVideo: null,
   }
 
+  componentDidMount() {
+    this.onTermSubmit('critical role')
+  }
+
   onTermSubmit = async (term) => {
     const response = await youtube.get('/search', {
       params: {
         q: term,
       },
     })
-    this.setState({ videos: response.data.items })
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0],
+    })
   }
 
   onVideoSelect = (video) => {
@@ -29,10 +36,10 @@ class App extends React.Component {
         <SearchBar onFormSubmit={this.onTermSubmit} />
         <div className="ui grid">
           <div className="ui row">
-            <div className='eleven wide column'>
+            <div className="eleven wide column">
               <VideoDetail video={this.state.selectedVideo} />
             </div>
-            <div className='five wide column'>
+            <div className="five wide column">
               <VideoList
                 videos={this.state.videos}
                 onVideoSelect={this.onVideoSelect}
